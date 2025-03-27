f0611e9fef3c45b9fa703db1e60cc94a55cc4bb7
sarath482 <rsarathkumar482@gmail.com>
2025-03-27 11:05:36 +0530

    git log commnet are added

---
+++ shellpratice.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##################################
+#Shell Script pratice Examples
+#Author:Sarath
+#Date:27032024
+#Version:V1
+#################################
+
+echo "hello , World"
+echo "Below are the basic git commands "
+#git init
+#git add
+#git add .
+#git status
+#git commit -m "commit message name "
+echo "git init,git status, git add, git add ., git branch ,git checkout master,git log"
