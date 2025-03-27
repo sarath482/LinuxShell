e1d7104f352505f093e52d964986b5fdf338c88d
sarath482 <rsarathkumar482@gmail.com>
2025-03-27 11:25:16 +0530

    git diffrence commmands are added

--- shellpratice.sh
+++ shellpratice.sh
@@ -14,3 +14,4 @@ echo "Below are the basic git commands "
 #git status
 #git commit -m "commit message name "
 echo "git init,git status, git add, git add ., git branch ,git checkout master,git log"
+echo "git new commands are added i.e git ls-tree -r master --name-only ,git ls-tree -r dev --name-only,remoove file in git (git rm filename)"
